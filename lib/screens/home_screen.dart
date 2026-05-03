686e8f24c3303a189fb374ce51eb73645b95ab30
zeilyangelia16 <zeilyangelia@gmail.com>
2026-05-03 23:20:22 +0700

    Membuat tampilan awal home screen (saldo dan daftar transaksi)

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Smart CashNote")),
+      body: Column(
+        children: [
+          const SizedBox(height: 20),
+
+          const Text("Total Saldo", style: TextStyle(fontSize: 18)),
+          const Text(
+            "Rp 1.000.000",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          Expanded(
+            child: ListView(
+              children: const [
+                ListTile(
+                  title: Text("Makan"),
+                  subtitle: Text("Pengeluaran"),
+                  trailing: Text("-20.000"),
+                ),
+                ListTile(
+                  title: Text("Gaji"),
+                  subtitle: Text("Pemasukan"),
+                  trailing: Text("+500.000"),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
